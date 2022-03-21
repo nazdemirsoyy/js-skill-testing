c0020f26881c6d31b9dc0463eedb61f1456bde3d
Naz Demirsoy <75198796+pandacornnny@users.noreply.github.com>
2022-03-21 21:18:10 +0100

    Delete clock.js

--- clock.js
+++
@@ -1,6 +0,0 @@
-const getName = prompt(`isminizi giriniz`);
-document.querySelector("#myName").textContent = getName;
-
-let time = new Date();
-let currentTime = `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}  ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
-document.querySelector("#myClock").textContent = currentTime;
